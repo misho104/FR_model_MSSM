ae13df5be353027cad96a2f1781dfb52ce400fb9
Sho Iwamoto <contact@misho-web.com>
2018-07-10 12:07:37 +0200

    feat: add noLFV_noCPV model and noCPV model

---
+++ MSSM_nocpv_UFO/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.32
+# Mathematica version: 11.2.0 for Linux x86 (64-bit) (September 11, 2017)
+# Date: Sat 7 Jul 2018 00:48:53
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
---
+++ MSSM_nocpv_UFO/coupling_orders.py
@@ -0,0 +1,16 @@
+# This file was automatically created by FeynRules 2.3.32
+# Mathematica version: 11.2.0 for Linux x86 (64-bit) (September 11, 2017)
+# Date: Sat 7 Jul 2018 00:40:55
+
+
+from object_library import all_orders, CouplingOrder
+
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
